1463ad9012ed309ffb56022dffca289359e3b144
Guilherme Isaías <guilherme.silva@ubistart.com>
2021-05-03 16:09:01 -0300

    Verificação do token de autenticação

---
+++ src/adapters/controllers/CheckAuth.ts
@@ -0,0 +1,45 @@
+import { Knex } from "knex";
+import Controller from "./Controller";
+import * as yup from 'yup';
+import User from "../../entities/User";
+import UserRepository from "../models/User";
+
+export interface CheckAuthInterface {
+    token: string;
+}
+
+export interface CheckAuthResponse {
+    isValid: boolean;
+    user?: User;
+}
+
+const schema = yup.object().shape({
+    token: yup.string().required('O token de autenticação é obrigatório'),
+})
+
+export default class CheckAuthController extends Controller<CheckAuthInterface> {
+    constructor(data: CheckAuthInterface, driver: Knex) {
+        super(data, schema, driver);
+    }
+
+    private async retrieveUser(token: string): Promise<User> {
+        const repository = new UserRepository(this.driver);
+        const user = await repository.query
+            .innerJoin('login_sessions', 'users.user_id', '=', 'login_sessions.user_id')
+            .where('login_sessions.session_id', token)
+            .first();
+        if (!user) throw new Error('Token inválido');
+        return repository.toEntity(user);
+    }
+
+    public async check(): Promise<CheckAuthResponse> {
+        try {
+            const data = await this.validate();
+            const user = await this.retrieveUser(data.token);
+            return { isValid: true, user }
+        } catch (error) {
+            return { isValid: false }
+        }
+    }
+
+}
