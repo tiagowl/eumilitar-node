118ce587ff488d49d8d443020e17c78c0ae5a735
Guilherme Isaías <guilherme.silva@ubistart.com>
2021-05-04 17:55:22 -0300

    Testes na entidade de tema

--- tests/entities.test.ts
+++ tests/entities.test.ts
@@ -1,6 +1,7 @@
 import User from '../src/entities/User'
 import bcrypt from 'bcrypt';
 import { hashPasswordSync, now, userEntityFactory } from './shortcuts';
+import EssayTheme from '../src/entities/EssayTheme';
 
 test('Testes na entidade User', () => {
     const password = 'l23jlk234';
@@ -15,4 +16,26 @@ test('Testes na entidade User', () => {
     expect(user.lastName).toBe('Antonio');
     expect(user.email).toBe('email@gmail.com');
     expect(user.lastModified).not.toBe(now);
+})
+
+test('Testes na entidade EssayTheme', () => {
+    const theme = new EssayTheme({
+        id: 1,
+        title: 'Tema da redação',
+        helpText: 'Texto de ajuda',
+        file: '/var/lib/app/data/file.pdf',
+        startDate: now,
+        endDate: now,
+        lastModified: now,
+        courses: ['esa']
+    })
+    expect(theme.id).toBe(1)
+    expect(theme.title).toBe('Tema da redação')
+    expect(theme.helpText).toEqual('Texto de ajuda')
+    expect(theme.file).toEqual('/var/lib/app/data/file.pdf')
+    theme.update({
+        title: 'Novo título'
+    })
+    expect(theme.title).toEqual('Novo título')
+    expect(theme.lastModified).not.toEqual(now)
 })
