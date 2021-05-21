3004275acbd163584b6cb9d95612961231796da3
Guilherme Isaías <guilherme.silva@ubistart.com>
2021-05-21 15:33:16 -0300

    Modificação na tabela das redações

--- migrations/20210520191303_essay.ts
+++ migrations/20210520191303_essay.ts
@@ -2,23 +2,47 @@ import { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
-    return knex.schema.alterTable('essays', (table) => {
+    const schema = knex.schema;
+    const exists = await schema.hasTable('essays');
+    return (exists ? schema :
+        schema.createTable('essays', table => {
+            table.increments('essay_id').index()
+                .notNullable();
+            table.string('file_name', 255).notNullable();
+            table.string('file_url', 200).notNullable();
+            table.string('file_path', 255).notNullable();
+            table.integer('user_id', 11).unsigned()
+                .references('user_id').inTable('users')
+                .onDelete('CASCADE').onUpdate('CASCADE')
+                .notNullable();
+            table.integer('course_tag', 1).notNullable();
+            table.dateTime('sent_date').notNullable()
+        })
+    ).alterTable('essays', (table) => {
         table.boolean('local').defaultTo(true)
             .notNullable();
         table.dateTime('last_modified').notNullable()
             .defaultTo(knex.fn.now());
         table.integer('theme').unsigned()
-            .references('essay_themes').inTable('users')
+            .references('id').inTable('essay_themes')
             .onDelete('CASCADE').onUpdate('CASCADE')
             .nullable();
         table.string('status', 15).nullable();
+        table.string('file_name').defaultTo('')
+            .nullable().alter();
+        table.dateTime('sent_date').notNullable()
+            .defaultTo(knex.fn.now()).alter()
     })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
-    return knex.schema.alterTable('essay', table => {
+    return knex.schema.alterTable('essays', table => {
+        table.dropForeign('theme');
         table.dropColumn('local');
         table.dropColumn('last_modified');
+        table.dropColumn('theme');
+        table.dropColumn('status');
+        table.string('file_name', 255).notNullable().alter();
     });
 }
