e456ea820d79fecf272ee52387a42d2de0bb20de
Guilherme Isaías <guilherme.silva@ubistart.com>
2021-07-08 09:56:06 -0300

    Ajustes nas migrations

--- migrations/20210602193848_correction.ts
+++ migrations/20210602193848_correction.ts
@@ -39,7 +39,5 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 
-export async function down(knex: Knex): Promise<void> {
-    return knex.schema.dropTable('essay_grading');
-}
+export async function down(knex: Knex): Promise<void> { }
 
