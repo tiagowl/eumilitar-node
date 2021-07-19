c5a3ea32e6b6302b773b776345d215d8fb098e00
Guilherme Isaías <guilherme.silva@ubistart.com>
2021-07-19 10:54:28 -0300

    Nova formatação dos logs

--- src/drivers/logger.ts
+++ src/drivers/logger.ts
@@ -2,9 +2,9 @@ import winston from 'winston';
 
 export default function create(settings: winston.LoggerOptions) {
     const logger = winston.createLogger(settings);
-    logger.error = error => {
+    logger.error = (error: any) => {
         if (error instanceof Error) {
-            logger.log({ level: 'error', message: `${error.stack || Object.assign({}, error)}` });
+            logger.log({ level: 'error', message: error.stack || error.message });
         } else {
             logger.log({ level: 'error', message: error });
         }
