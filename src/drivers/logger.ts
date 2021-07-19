97642af9534674b0c1530cb7ab6f158e33ec8d85
Guilherme Isaías <guilherme.silva@ubistart.com>
2021-07-19 10:32:05 -0300

    Formatação dos erros no log

--- src/drivers/logger.ts
+++ src/drivers/logger.ts
@@ -1,5 +1,14 @@
 import winston from 'winston';
 
 export default function create(settings: winston.LoggerOptions) {
-    return winston.createLogger(settings);
+    const logger = winston.createLogger(settings);
+    logger.error = error => {
+        if (error instanceof Error) {
+            logger.log({ level: 'error', message: `${error.stack || Object.assign({}, error)}` });
+        } else {
+            logger.log({ level: 'error', message: error });
+        }
+        return logger;
+    };
+    return logger;
 }
