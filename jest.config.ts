0f8e76fecf30100db1680ab1d6d2259463ea266f
Guilherme Isaías <guilherme.silva@ubistart.com>
2021-07-19 11:06:44 -0300

    Ajustes nos testes

--- jest.config.ts
+++ jest.config.ts
@@ -114,12 +114,13 @@ export default {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: "tests/",
+  // rootDir: "tests/",
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "tests/"
-  // ],
+  roots: [
+    "tests/",
+    "src/"
+  ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
