f5eb126a59151e5e058d56780d830b28cc250912
Guilherme Isaías <guilherme.silva@ubistart.com>
2021-07-20 16:02:36 -0300

    Ajustes nos logs

--- src/drivers/logger.ts
+++ src/drivers/logger.ts
@@ -4,7 +4,7 @@ export default function create(settings: winston.LoggerOptions) {
     const logger = winston.createLogger(settings);
     logger.error = (error: any) => {
         if (error instanceof Error) {
-            logger.log({ level: 'error', message: error.stack || error.message });
+            logger.log({ level: 'error', message: error.stack || String(error) });
         } else {
             logger.log('error', { error, stack: new Error().stack });
         }
