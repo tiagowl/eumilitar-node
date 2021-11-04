c646869788430c145ed34049b12dc705618c7ffb
Guilherme Isaías <guilherme.silva@ubistart.com>
2021-11-04 12:16:10 -0300

    Entidade dos logs

--- src/entities/Log.ts
+++ src/entities/Log.ts
@@ -1,19 +1,27 @@
+export type EventType = 'login' | 'sent-essay' | 'theme-preview' | 'correction-preview' | 'single-link-sent';
+
+export const eventTypes = new Set<EventType>(['login', 'sent-essay', 'theme-preview', 'correction-preview', 'single-link-sent']);
+
 export interface LogInterface {
     readonly id: number;
     user?: number;
     registrationDate: Date;
-    event: string;
+    event: EventType;
     userAgent: string;
     ip: string;
+    error?: string;
+    details?: string;
 }
 
 export default class Log implements LogInterface {
     public readonly id: number;
     public user?: number;
     public registrationDate: Date;
-    public event: string;
+    public event: EventType;
     public userAgent: string;
     public ip: string;
+    public error?: string;
+    public details?: string;
 
     constructor(data: LogInterface) {
         this.id = data.id;
@@ -22,5 +30,7 @@ export default class Log implements LogInterface {
         this.event = data.event;
         this.userAgent = data.userAgent;
         this.ip = data.ip;
+        this.error = data.error;
+        this.details = data.details;
     }
 }
--- src/entities/__tests__/log.test.ts
+++ src/entities/__tests__/log.test.ts
@@ -10,6 +10,8 @@ test('Recuperação de senha', () => {
             userAgent: faker.internet.userAgent(),
             ip: faker.internet.ip(),
             event: 'login',
+            details: faker.lorem.lines(4),
+            error: faker.lorem.lines(1),
         });
     }).not.toThrowError();
 });
