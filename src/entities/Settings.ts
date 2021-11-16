fa3ee880d194ed8b330e5f6dd9231f1634d6a99e
Guilherme Isaías <guilherme.silva@ubistart.com>
2021-11-16 11:06:11 -0300

    Entidade das configurações

---
+++ src/entities/Settings.ts
@@ -0,0 +1,18 @@
+export interface SettingsInterface {
+    readonly id: number;
+    lastModified: Date;
+    reviewExpiration: number;
+
+}
+
+export default class Settings implements SettingsInterface {
+    public readonly id: number;
+    public lastModified: Date;
+    public reviewExpiration: number;
+
+    constructor(data: SettingsInterface) {
+        this.id = data.id;
+        this.lastModified = data.lastModified;
+        this.reviewExpiration = data.reviewExpiration;
+    }
+}
---
+++ src/entities/__tests__/settings.test.ts
@@ -0,0 +1,12 @@
+import faker from "faker";
+import Settings from "../Settings";
+
+test('Recuperação de senha', () => {
+    expect(() => {
+        return new Settings({
+            id: faker.datatype.number(),
+            lastModified: new Date(),
+            reviewExpiration: faker.datatype.number(),
+        });
+    }).not.toThrowError();
+});
