10799ef65da83a59d263b342d28124e3586dd7e0
Guilherme Isaías <guilherme.silva@ubistart.com>
2021-12-16 17:49:19 -0300

    Teste

--- src/cases/__tests__/repositories/UserTestRepository.ts
+++ src/cases/__tests__/repositories/UserTestRepository.ts
@@ -1,8 +1,9 @@
 import User, { UserInterface } from "../../../entities/User";
+import { ChartFilter } from "../../interfaces";
 import { UserRepositoryInterface, UserFilter, UserSavingData } from "../../UserCase";
 import { FakeDB } from "./database";
 import TestRepository from "./TestRepository";
-
+import SubscriptionTestRepository from "./SubscriptionTestRepository";
 
 export default class UserTestRepository extends TestRepository<User, UserInterface> implements UserRepositoryInterface {
     constructor(db: FakeDB) {
@@ -11,5 +12,15 @@ export default class UserTestRepository extends TestRepository<User, UserInterfa
 
     async all() {
         return this.database;
+    }
+
+    public async countActives(filter: ChartFilter<UserInterface>) {
+        const { period, ...filterData } = filter;
+        const subscriptions = new SubscriptionTestRepository(this.db);
+        const filtered = await this.filter(filterData);
+        const subscriptionsCount = await Promise.all(filtered.map(async (user) => {
+            return subscriptions.count({ 'user': user.id });
+        }));
+        const actives = subscriptionsCount.filter(val => val > 0);
     }
 }
